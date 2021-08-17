9439de0c5d2c7f716c73e554ac7c13527b14a9fc
Long <long0dai@foxmail.com>
2021-08-17 13:10:44 +0000

    feedback
    Signed-off-by: Long <long0dai@foxmail.com>

--- agent/pkg/dns/dns.go
+++ agent/pkg/dns/dns.go
@@ -1,14 +1,17 @@
 package dns
 
 import (
+	"io/ioutil"
 	"net"
-
-	"k8s.io/klog/v2"
-
-	"github.com/kubeedge/edgemesh/agent/pkg/dns/controller"
-	"github.com/kubeedge/edgemesh/common/util"
+	"strings"
+	"time"
 
 	mdns "github.com/miekg/dns"
+	"k8s.io/klog/v2"
+
+	beehiveContext "github.com/kubeedge/beehive/pkg/core/context"
+	"github.com/kubeedge/edgemesh/agent/pkg/dns/controller"
+	"github.com/kubeedge/edgemesh/common/util"
 )
 
 const hostResolv = "/etc/resolv.conf"
@@ -24,8 +27,8 @@ func (h *handler) ServeDNS(w mdns.ResponseWriter, r *mdns.Msg) {
 		domain := msg.Question[0].Name
 		address, ok := lookup(domain)
 		if ok {
-			msg.Answer = append(msg.Answer, &dns.A{
-				Hdr: dns.RR_Header{Name: domain, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 60},
+			msg.Answer = append(msg.Answer, &mdns.A{
+				Hdr: mdns.RR_Header{Name: domain, Rrtype: mdns.TypeA, Class: mdns.ClassINET, Ttl: 60},
 				A:   net.ParseIP(address),
 			})
 		} else {
@@ -38,6 +41,21 @@ func (h *handler) ServeDNS(w mdns.ResponseWriter, r *mdns.Msg) {
 }
 
 func (dns *EdgeDNS) Run() {
+	// ensure /etc/resolv.conf have dns nameserver
+	go func() {
+		dns.ensureResolvForHost()
+		ticker := time.NewTicker(time.Minute)
+		for {
+			select {
+			case <-ticker.C:
+				dns.ensureResolvForHost()
+			case <-beehiveContext.Done():
+				dns.cleanResolvForHost()
+				return
+			}
+		}
+	}()
+
 	dns.Server.Handler = &handler{}
 	if err := dns.Server.ListenAndServe(); err != nil {
 		klog.Errorf("dns server serve error: %v", err)
@@ -54,4 +72,106 @@ func lookup(serviceURL string) (ip string, exist bool) {
 	}
 	klog.Infof("dns server parse %s ip %s", serviceURL, ip)
 	return ip, true
+}
+
+// ensureResolvForHost adds edgemesh dns server to the head of /etc/resolv.conf
+func (dns *EdgeDNS) ensureResolvForHost() {
+	bs, err := ioutil.ReadFile(hostResolv)
+	if err != nil {
+		klog.Errorf("read file %s err: %v", hostResolv, err)
+		return
+	}
+
+	resolv := strings.Split(string(bs), "\n")
+	if resolv == nil {
+		nameserver := "nameserver " + dns.ListenIP.String()
+		if err := ioutil.WriteFile(hostResolv, []byte(nameserver), 0600); err != nil {
+			klog.Errorf("write file %s err: %v", hostResolv, err)
+		}
+		return
+	}
+
+	configured := false
+	dnsIdx := 0
+	startIdx := 0
+	for idx, item := range resolv {
+		if strings.Contains(item, dns.ListenIP.String()) {
+			configured = true
+			dnsIdx = idx
+			break
+		}
+	}
+	for idx, item := range resolv {
+		if strings.Contains(item, "nameserver") {
+			startIdx = idx
+			break
+		}
+	}
+	if configured {
+		if dnsIdx != startIdx && dnsIdx > startIdx {
+			nameserver := sortNameserver(resolv, dnsIdx, startIdx)
+			if err := ioutil.WriteFile(hostResolv, []byte(nameserver), 0600); err != nil {
+				klog.Errorf("failed to write file %s, err: %v", hostResolv, err)
+				return
+			}
+		}
+		return
+	}
+
+	nameserver := ""
+	for idx := 0; idx < len(resolv); {
+		if idx == startIdx {
+			startIdx = -1
+			nameserver = nameserver + "nameserver " + dns.ListenIP.String() + "\n"
+			continue
+		}
+		nameserver = nameserver + resolv[idx] + "\n"
+		idx++
+	}
+
+	if err := ioutil.WriteFile(hostResolv, []byte(nameserver), 0600); err != nil {
+		klog.Errorf("failed to write file %s, err: %v", hostResolv, err)
+		return
+	}
+}
+
+func sortNameserver(resolv []string, dnsIdx, startIdx int) string {
+	nameserver := ""
+	idx := 0
+	for ; idx < startIdx; idx++ {
+		nameserver = nameserver + resolv[idx] + "\n"
+	}
+	nameserver = nameserver + resolv[dnsIdx] + "\n"
+
+	for idx = startIdx; idx < len(resolv); idx++ {
+		if idx == dnsIdx {
+			continue
+		}
+		nameserver = nameserver + resolv[idx] + "\n"
+	}
+
+	return nameserver
+}
+
+// cleanResolvForHost delete edgemesh dns server from the head of /etc/resolv.conf
+func (dns *EdgeDNS) cleanResolvForHost() {
+	bs, err := ioutil.ReadFile(hostResolv)
+	if err != nil {
+		klog.Warningf("read file %s err: %v", hostResolv, err)
+	}
+
+	resolv := strings.Split(string(bs), "\n")
+	if resolv == nil {
+		return
+	}
+	nameserver := ""
+	for _, item := range resolv {
+		if strings.Contains(item, dns.ListenIP.String()) || item == "" {
+			continue
+		}
+		nameserver = nameserver + item + "\n"
+	}
+	if err := ioutil.WriteFile(hostResolv, []byte(nameserver), 0600); err != nil {
+		klog.Errorf("failed to write nameserver to file %s, err: %v", hostResolv, err)
+	}
 }
