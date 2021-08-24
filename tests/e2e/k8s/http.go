51af490254294a754634daa69f9753745ad4376c
khalid-davis <qinkai1994@qq.com>
2021-08-24 17:04:43 +0800

    e2e: add e2e framework and support ci e2e (#34)
    This commit adds a e2e framework for the edgemesh project, implement serveral basic test case including dns  resolve, tcp traffic, http traffic. The ci e2e also is supported too.
    
    Signed-off-by: khalid-jobs <qinkai1994@qq.com>

---
+++ tests/e2e/k8s/http.go
@@ -0,0 +1,37 @@
+package k8s
+
+import (
+	"bytes"
+	"crypto/tls"
+	"net/http"
+	"time"
+
+	"github.com/onsi/ginkgo"
+
+	"github.com/kubeedge/kubeedge/tests/e2e/utils"
+)
+
+func handlePostRequest2K8s(url string, body []byte) error {
+	var req *http.Request
+	var err error
+
+	defer ginkgo.GinkgoRecover()
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{
+		Transport: tr,
+	}
+	req, err = http.NewRequest(http.MethodPost, url, bytes.NewBuffer(body))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	t := time.Now()
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	utils.Infof("%s %s %v in %v", req.Method, req.URL, resp.Status, time.Since(t))
+	return nil
+}
