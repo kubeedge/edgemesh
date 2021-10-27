cfabfe0cc989a52b1bb9a60403c6bbbdce828eea
Poorunga <2744323@qq.com>
2021-10-27 15:07:51 +0800

    create README.md symbolic link
    Signed-off-by: Poorunga <2744323@qq.com>

--- docs/.vuepress/clientAppEnhance.ts
+++
@@ -1,9 +0,0 @@
-import { defineClientAppEnhance } from '@vuepress/client'
-
-const enGuide = {path: '/', redirect: '/guide/'}
-const zhGuide = {path: '/zh', redirect: '/zh/guide/'}
-
-export default defineClientAppEnhance(({ app, router, siteData }) => {
-  router.addRoute(enGuide)
-  router.addRoute(zhGuide)
-})
--- docs/.vuepress/configs/sidebar/zh.ts
+++ docs/.vuepress/configs/sidebar/zh.ts
@@ -1,16 +1,21 @@
 import type { SidebarConfig } from '@vuepress/theme-default'
 
+const guide = {
+  text: '指南',
+  children: [
+    '/zh/guide/README.md',
+    '/zh/guide/getting-started.md',
+    '/zh/guide/test-case.md',
+    '/zh/guide/edge-gateway.md',
+  ],
+}
+
 export const zh: SidebarConfig = {
+  '/zh/': [
+    guide,
+  ],
   '/zh/guide/': [
-    {
-      text: '指南',
-      children: [
-        '/zh/guide/README.md',
-        '/zh/guide/getting-started.md',
-        '/zh/guide/test-case.md',
-        '/zh/guide/edge-gateway.md',
-      ],
-    },
+    guide,
   ],
   '/zh/advanced/': [
     {
